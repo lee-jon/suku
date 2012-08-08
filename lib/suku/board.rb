7c5089461a3d7d2fbae28342b0a47a640d470fe9
Lee-Jon <leejonball@gmail.com>
2012-08-08 12:06:56 +0100

    Add defensive test of Board class

--- lib/suku/board.rb
+++ lib/suku/board.rb
@@ -1,6 +1,7 @@
 module Sudoku
   class Board
     def initialize(input=nil)
+
       if input.nil?
         @grid = Array.new
         (0..8).each do |i|
@@ -8,6 +9,7 @@ module Sudoku
         end
 
       else #convert an input string
+        raise ArgumentError.new("Invalid number of characters in String input") unless input.length == 81
         @grid = Array.new(9)
         x = []; input.each_char { |char| x << char }
         (0..8).each do |row|
@@ -23,5 +25,16 @@ module Sudoku
     def stream
       @grid.join
     end
+
+    def show
+      response = ""
+      (0..8).each do |i|
+        response += "#{@grid[i][0]} #{@grid[i][1]} #{@grid[i][2]} | #{@grid[i][3]} #{@grid[i][4]} #{@grid[i][5]} | #{@grid[i][6]} #{@grid[i][7]} #{@grid[i][8]}\n".gsub("0", ".")
+        if i == 2 || i == 5
+          response += "------+-------+------\n"
+        end
+      end
+    end
+
   end
 end
--- spec/suku/board_spec.rb
+++ spec/suku/board_spec.rb
@@ -2,10 +2,45 @@ require File.join(File.dirname(__FILE__), "/../spec_helper")
 
 module Sudoku
   describe Board do
+
     context "starting up" do
-      it "with no input a blank board is created" do
-        game = Board.new
-        game.stream.should include("0"*81)
+      describe "with no input" do
+        it "should create a blank board" do
+          game = Board.new
+          game.stream.should eq("0"*81)
+        end
+      end
+
+      describe "with a valid string input" do
+        it "should create a valid board" do
+          game = Board.new("123456789"*9)
+          game.stream.should eq("123456789"*9)
+        end
+      end
+
+      describe "with an invalid input" do
+        it "should not create a game if the characters are not 81 in length" do
+          lambda { Board.new("123") }.should raise_error(ArgumentError)
+        end
+      end
+    end
+
+    context "viewing the board" do
+      before(:each) do
+        @game = Board.new
+      end
+
+      describe "by requesting a string" do
+        it "should output a string of 81 characters" do
+          @game.should respond_to(:stream)
+          @game.stream.should have(81).characters
+        end
+      end
+
+      describe "by requesting a board view on standard out" do
+        it "should output to STDOUT" do
+          @game.should respond_to(:show)
+        end
       end
     end
 
